2355ab6bb9c5249241e3da37400654ea09dec04c
Muhammad Awais <muhammadawaiss1216@gmail.com>
2026-09-02 13:06:18 +0500

    Add plant product listing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import AboutUs from "./AboutUs";
+import ProductList from "./ProductList";
 
 function App() {
   return (
     <div className="app">
       <h1>Paradise Nursery</h1>
+
       <AboutUs />
+
+      <ProductList />
     </div>
   );
 }
